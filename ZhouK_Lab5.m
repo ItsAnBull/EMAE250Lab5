24d14570858a59661a7c10566998c36e25d541e8
Kevin <purplefirepokemon@gmail.com>
2025-10-01 18:44:17 -0400

    nothing got done

--- ZhouK_Lab5.m
+++ ZhouK_Lab5.m
@@ -1,3 +1,17 @@
-function output = ZhouK_Lab5(data, target, epsilon)
+function output = ZhouK_Lab5(input, target, target_epsilon)
 
-table = trans(data(:,1));
+% get the candidate x values in a horizontal vector
+data = transpose(input(:,1))
+
+% initialize the table
+table = zeros(length(data));
+
+d1 = abs(data(1)-target);
+d2 = abs(data(2)-target);
+i = 2;
+while (d2 < d1)
+    i = i + 1;
+    d1 = d2;
+    d2 = abs(data(i)-target);    
+end
+i
